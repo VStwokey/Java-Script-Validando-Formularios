1f12e51627dc1bbf1053600574b28a2e88f81a93
Vinicius Santana <Vinicius.Lima33@Outlook.com>
2023-08-08 18:29:16 -0300

    validação de cpf e de idade.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+import UmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+})
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11){
+        UmCPF(campo);
+    } if (campo.name == "aniversario" && campo.value != ""){
+        ehMaiorDeIdade(campo);
+    }
+}
